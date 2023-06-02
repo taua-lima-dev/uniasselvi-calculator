0a61769726c0ec78e7d37117a769c792ca04b3e7
Tauã Lima <taua.lima.dev@gmail.com>
2023-06-02 16:11:00 -0300

    finished v1.0

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CONSTANTES E VARIÁVEIS
 const entradaA1 = document.querySelector("#entradaA1");
 const entradaA2 = document.querySelector("#entradaA2");
 const entradaA3 = document.querySelector("#entradaA3");
@@ -12,22 +13,24 @@ let mediaFinal = "";
 
 const enviar = document.querySelector("#calcular");
 
+// ALGORÍTIMO 
+
 enviar.addEventListener("click", function (e) {
   e.preventDefault();
   
   if (
     entradaA1.value == "" || entradaA2.value == "" || entradaA3.value == "" || entradaA4.value == "") {
     alert("Preencha todos os campos");
-  } else if (entradaA5.value === "true") {
-    alert("obaaaaaa")
-    console.log(entradaA5)
+
+  } else if (entradaA5.value !== "") {
+    mediaSimulado()
+    escondido.classList.remove("oculto");
+    removerFundo();
   }
   else {
     mediaBase()
     escondido.classList.remove("oculto");
-    removeContent()
-    console.log(entradaA5.value);
-    
+    removerFundo()   
   }
 
   if (mediaFinal < 7){
@@ -37,7 +40,8 @@ enviar.addEventListener("click", function (e) {
   }
 });
 
-const removeContent = () => {
+// FUNÇOES
+const removerFundo = () => {
   enviar.classList.add("oculto")
   form.classList.add("oculto");
 }
@@ -51,12 +55,30 @@ const mediaBase = () => {
   ).toFixed(2);
 }
 
+const mediaSimulado = () => {
+  mediaFinal = (
+    Number(entradaA1.value) * 0.15 +
+    Number(entradaA2.value) * 0.15 +
+    Number(entradaA3.value) * 0.4 +
+    Number(entradaA4.value) * 0.3 +
+    Number(entradaA5.value) * 0.05
+  ).toFixed(2);
+  }
+
 const aprovado = () => {
-  media.style.color = "#0c6140";
-  resultado.innerText = `${mediaFinal}`;
-  resultado.style.color = "#0c6140";
-  aviso.innerHTML = `Você está <strong>APROVADO</strong>`;
-  aviso.style.color = "#0c6140";
+  if (mediaFinal >= 10.00) {
+    media.style.color = "#0c6140";
+    resultado.innerText = `10.00`;
+    resultado.style.color = "#0c6140";
+    aviso.innerHTML = `Você está <strong>APROVADO</strong>`;
+    aviso.style.color = "#0c6140";
+  } else {
+    media.style.color = "#0c6140";
+    resultado.innerText = `${mediaFinal}`;
+    resultado.style.color = "#0c6140";
+    aviso.innerHTML = `Você está <strong>APROVADO</strong>`;
+    aviso.style.color = "#0c6140";
+  }
 }
 
 const reprovado = () => {
